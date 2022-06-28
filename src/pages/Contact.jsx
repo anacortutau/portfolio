fdb81cd38ee1a6883eee2f9370ec661463802da8
anacortutau <anacortutau@gmail.com>
2022-06-29 00:25:35 +0200

    portfolio email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,27 +19,12 @@ function Contact() {
 
   }
   
-  const handleSubmit = (e)=>{
-    e.preventDefault();
-    console.log(formRef.current)
-    emailjs.sendForm(
-      'service_zcn7c2w', 
-      'template_kze37kh', formRef.current, 'ZdMAxBoOpbhjImlaC')
-    .then((result) => {
-        console.log(result.text);
-        setDone(true)
-        setDataEmail({user_name: "", user_email: "", message: ""})
-    }, (error) => {
-        console.log(error.text);
-    });
-
-  }
   return (
     <div className="c">
       <div className="c-bg">
       <div className="c-wrapper">
         <div className="c-left">
-          <h1 className="c-title"> Contact with me </h1>
+          <h1 className="c-title"> Contact me </h1>
           <div className="c-info">
             <div className="c-info-item">
               <img className="c-icon" src={Phone} alt="" />
@@ -50,16 +35,6 @@ function Contact() {
               anacortutau@gmail.com
             </div>
           </div>
-          <form  ref={formRef} onSubmit={handleSubmit}>
-            <input type="text" placeholder="Name" name="user_name" onChange={handleInput} value={dataEmail.user_name}/>
-            <br />
-            <input type="text" placeholder="Email" name="user-email" onChange={handleInput} value={dataEmail.user_email}/>
-            <br />
-            <textarea rows="5" placeholder="Message" name="message" onChange={handleInput} value={dataEmail.message}/>
-            <br />
-            <button>Submit</button>
-            {done && "Thank you"}
-          </form>
         </div>
       </div>
       </div>
